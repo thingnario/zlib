0a086cfb97ab6fa85e2eafde22c829cad845772e
eric11220 <b99902080@ntu.edu.tw>
2017-09-07 21:13:00 +0800

    build_zlib.sh

---
+++ build_zlib.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./build_zlib.sh target_architecture!"
+    echo "Example: ./build_zlib.sh arm-linux"
+    exit
+fi
+
+tool_chain_path=${1%/}
+
+ARCH=`echo $tool_chain_path | awk -F"/" '{print (NF>1)? $NF : $tool_chain_path}'`
+export CHOST="$ARCH"
+
+./configure --prefix=`pwd`/final --static
+make 
+make install
+
+cd final
+cp -r * $tool_chain_path
